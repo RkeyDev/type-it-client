b93ca3fec91697262579f482b8787578bef728ef
rkeyDev <roei1576@gmail.com>
2025-10-12 14:25:48 +0300

    Updated socket connection to koyeb server

--- src/pages/socket.js
+++ src/pages/socket.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket("ws://localhost:8080");
+const socket = new WebSocket("wss//respectable-crow-rkeydev-11f1364e.koyeb.app/");
 
 //wss//respectable-crow-rkeydev-11f1364e.koyeb.app/
 //ws://localhost:8080
